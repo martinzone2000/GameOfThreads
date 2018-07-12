76506e5c8b474883ad7ca6299805dd747cabc3e3
OT-AGonzalez <agonzalez@onetechnologies.net>
2018-07-12 16:47:34 -0500

    Adding the basic template for routing

---
+++ got-app/src/components/Trophy/Trophy.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Trophy.css';
+
+class Trophy extends Component {
+  render() {
+    return (
+      <div className="Trophy">
+        <h1>Trophy Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Trophy;
