11bbf75b99131bee5c0c246e42be8965f3a92fd8
OT-AGonzalez <agonzalez@onetechnologies.net>
2018-07-12 18:21:17 -0500

    added dummy images and diplayed them in the trophy page

--- got-app/src/components/Trophy/Trophy.js
+++ got-app/src/components/Trophy/Trophy.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './Trophy.css';
+import Gallery from '../Gallery/Gallery.js';
 
 class Trophy extends Component {
   render() {
+    const images = [
+      "/images/badges/score1.png",
+      "/images/badges/score2.png",
+      "/images/badges/score3.png",
+      "/images/badges/score1.png",
+      "/images/badges/score2.png",
+      "/images/badges/score3.png"
+    ];
     return (
       <div className="Trophy">
         <h1>Trophy Page</h1>
+        <Gallery imageUrls={images} />
       </div>
     );
   }
