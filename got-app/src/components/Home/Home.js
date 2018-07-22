4966da34fb6871cb4aaab19627acf582eeeaade1
Jeff Martin <jmartin@samiam.local>
2018-07-22 14:00:55 -0500

    move badge data to global

--- got-app/src/components/Home/Home.js
+++ got-app/src/components/Home/Home.js
@@ -17,77 +17,48 @@ class Home extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      badge: new Badge(),
-      badgeCompendium : new BadgeCollection(),
-      customerBadges : new CustomerBadges(),
-      newCount : 0
-    }
-
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addTrophy());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addGoingUp());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addSuperStar());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addBlastOff());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addLoyalty());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addQuizMaster());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addRecruiter());
-    this.state.badgeCompendium.badgeCollection.push(this.state.badge.addTechnoMaster());
   }
 
   UpdateScores =  () => {
     var data = this.props.app.state.globalData;
     var currentReport = data.creditReports[data.currentReport]
-    this.state.customerBadges.updateBadgeCollection(currentReport);
+    data.customerBadges.updateBadgeCollection(currentReport);
 
     this.props.app.UpdateScores();
 
     var stringify = require('json-stable-stringify');
-    localStorage.setItem('customerBadges', stringify(this.state.customerBadges.customerBadgeCollection));
-    this.state.newCount = this.getNewCount();
-
-    this.setState( {
-      newCount: this.state.newCount 
-    })
-  }
-
-  getNewCount() {
-    var count = 0;
-    console.log(this.state.customerBadges.customerBadgeCollection);
-
-    for(var i=0; i < this.state.customerBadges.customerBadgeCollection.length; i++) {
-      if(this.state.customerBadges.customerBadgeCollection[i].isNew) {
-        count ++;
-      }
-    }
-    return count;
+    localStorage.setItem('customerBadges', stringify(data.customerBadges.customerBadgeCollection));
   }
 
 
-  componentWillMount() {
+  /* componentWillMount() {
     
+    var data = this.props.app.state.globalData;
     var stringify = require('json-stable-stringify');
     var badgeCompendium = localStorage.getItem('badgeCompendium');
     var customerBadges = localStorage.getItem('customerBadges');
 
+
     if(badgeCompendium == null) {
       
-      localStorage.setItem('badgeCompendium', stringify(this.state.badgeCompendium.badgeCollection));
+      localStorage.setItem('badgeCompendium', stringify(data.badgeCompendium.badgeCollection));
     } else {
       
-      this.state.badgeCompendium.badgeCollection = JSON.parse(badgeCompendium);
+      data.badgeCompendium.badgeCollection = JSON.parse(badgeCompendium);
     }
 
     if(customerBadges == null) {
       
-      localStorage.setItem('customerBadges', stringify(this.state.customerBadges.customerBadgeCollection));
+      localStorage.setItem('customerBadges', stringify(data.customerBadges.customerBadgeCollection));
     } else {
       
-      this.state.customerBadges.customerBadgeCollection = JSON.parse(customerBadges);
+      data.customerBadges.customerBadgeCollection = JSON.parse(customerBadges);
     }
 
-    this.state.newCount = this.getNewCount();
-  }
+    data.newCount = this.props.app.getNewCount();
+
+    //this.props.app.setState({globalData: data})
+  } */
 
   render() {
     console.log(this.props)
